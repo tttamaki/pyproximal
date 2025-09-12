501a28c83ad07524ba1ec18d95c61d0cc1312150
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2025-09-12 23:53:40 +0900

    pass test
    - tol: 1e-4 --> 1e-7
    - add docstring
    - add show parameter
    - lmd --> eta

--- pyproximal/optimization/ppxa.py
+++ pyproximal/optimization/ppxa.py
@@ -1,28 +1,76 @@
+import time
 from typing import Callable, List, Optional
 
 import numpy as np
-
 from pylops.utils.typing import NDArray
-from pylops.utils.backend import get_array_module
+from pylops.utils.backend import get_array_module, to_numpy
+
 from pyproximal.ProxOperator import ProxOperator
 
 
-def PPXA(
+def PPXA(  # pylint: disable=invalid-name
     prox_ops: List[ProxOperator],
     x0: NDArray | List[NDArray],
     tau: float,
-    lmd: float = 1.0,
+    eta: float = 1.0,
     weights: NDArray | List[float] | None = None,
-    niter: int = 10,
-    tol: Optional[float] = None,
+    niter: int = 1000,
+    tol: Optional[float] = 1e-7,
     callback: Optional[Callable[..., None]] = None,
     show: bool = False,
 ) -> NDArray:
-    r"""Parallel Proximal Algorithm (PPXA) for solving:
+    r"""Parallel Proximal Algorithm (PPXA) for solving
         :math:: \min_x  sum_{i=1}^m f_i(x)
         given proximal operators of :math:`f_i`.
 
+    Parameters
+    ----------
+    prox_ops : :obj:`List[ProxOperator]`
+        A list of proximable functions :math:`f_1, \ldots, f_m`.
+    x0 : :obj:`numpy.ndarray`
+        Initial vector
+    tau : :obj:`float`
+        Positive scalar weight
+    eta : :obj:`float`, optional
+        Relaxation parameter (must be between 0 and 2, 0 excluded).
+    weights : :obj:`np.ndarray` or :obj:`List[float]` or :obj:`None`, optional, default=None
+        Weights :math:`\sum_{i=1}^m w_i = 1, \ 0 < w_i < 1`,
+        Defaults to None, which means :math:`w_1 = \cdots = w_m = \frac{1}{m}.`
+    max_iter : :obj:`int`, optional, default=1000
+        The maximum number of iterations.
+    tol : :obj:`float`, optional, default=1e-7
+        Torrelance to stop the iteration.
+    callbacky : :obj:`bool`, optional
+        Modify callback signature to (``callback(x, y)``)
+        when ``callbacky=True``
+    show : :obj:`bool`, optional
+        Display iterations log
+
+    Returns
+    -------
+    x : :obj:`numpy.ndarray`
+        Inverted model
+
+
+    Notes
+    -----
+    TODO: write docstring
+
+
+
     """
+    if show:
+        tstart = time.time()
+        print(
+            "Parallel Proximal Algorithm\n"
+            "---------------------------------------------------------"
+        )
+        for i, prox_op in enumerate(prox_ops):
+            print(f"Proximal operator (f{i}): {type(prox_op)}")
+        print(f"tau = {tau:10e}\tniter = {niter:d}\n")
+        head = "   Itn       x[0]          J=sum_i f_i"
+        print(head)
+
     ncp = get_array_module(x0)
 
     m = len(prox_ops)
@@ -38,22 +86,32 @@ def PPXA(
 
     x = np.mean(y, axis=0)
     x_old = x.copy()
-    for _ in range(niter):
+
+    for iiter in range(niter):
 
         p = ncp.stack([prox_ops[i].prox(y[i], tau / w[i]) for i in range(m)])
-
         pn = np.sum(w[:, None] * p, axis=0)
-
-        y = y + lmd * (2 * pn - x - p)
-
-        x = x + lmd * (pn - x)
+        y = y + eta * (2 * pn - x - p)
+        x = x + eta * (pn - x)
 
         if callback is not None:
             callback(x)
+
+        if show:
+            if iiter < 10 or niter - iiter < 10 or iiter % (niter // 10) == 0:
+                pf = np.sum([prox_ops[i](x) for i in range(m)])
+                print(
+                    f"{iiter + 1:6d}  {np.real(to_numpy(x[0])):12.5e}  "
+                    f"{pf:10.3e}"
+                )
 
         if np.abs(x - x_old).max() < tol:
             break
 
         x_old = x
+
+    if show:
+        print(f"\nTotal time (s) = {time.time() - tstart:.2f}")
+        print("---------------------------------------------------------\n")
 
     return x
--- pytests/test_solver_ppxa.py
+++ pytests/test_solver_ppxa.py
@@ -22,7 +22,8 @@ def test_ppxa_with_admm(par: Dict[str, Any]) -> None:
     """Check equivalency of PPXA and ADMM
     when using a single regularization term
     """
-    np.random.seed()
+    rng = np.random.default_rng()
+
     n, m = par["n"], par["m"]
 
     # Define sparse model
@@ -30,27 +31,29 @@ def test_ppxa_with_admm(par: Dict[str, Any]) -> None:
     x[2], x[4] = 1, 0.5
 
     # Random mixing matrix
-    R = np.random.normal(0.0, 1.0, (n, m))
+    R = rng.normal(0.0, 1.0, (n, m))
     Rop = MatrixMult(R)
-    y = Rop @ x
-    l2 = L2(Op=Rop, b=y, niter=50, warm=True)
 
-    g = np.random.normal(0.0, 1.0, m)
-    l1 = L1(sigma=5e-1, g=g)
+    y = Rop @ x
+
+    l2 = L2(Op=Rop, b=y, niter=50, warm=True)
+    l1 = L1(sigma=5e-1)
 
     # Step size
     L = (Rop.H * Rop).eigs(1).real.item()
     tau = 0.5 / L
 
-    # DRS
-    xadmm, _ = ADMM(l2, l1, x0=np.zeros(m), tau=tau, niter=15000, show=True)
+    xadmm, _ = ADMM(
+        l2, l1, x0=np.zeros(m), tau=tau, niter=15000, show=True)
+    xppxa1 = PPXA(
+        [l2, l1], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-7)
 
-    # PPXA
-    xppxa = PPXA(
-        [l2, l1], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-4
-    )
+    assert_array_almost_equal(xadmm, xppxa1, decimal=2)
 
-    assert_array_almost_equal(xadmm, xppxa, decimal=2)
+    xppxa2 = PPXA(
+        [l1, l2], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-7)
+
+    assert_array_almost_equal(xadmm, xppxa2, decimal=2)
 
 
 @pytest.mark.parametrize("par", [(par1), (par2)])
@@ -58,7 +61,8 @@ def test_ppxa_with_drs(par: Dict[str, Any]) -> None:
     """Check equivalency of PPXA and DouglasRachfordSplitting
     when using a single regularization term
     """
-    np.random.seed()
+    rng = np.random.default_rng()
+
     n, m = par["n"], par["m"]
 
     # Define sparse model
@@ -66,24 +70,26 @@ def test_ppxa_with_drs(par: Dict[str, Any]) -> None:
     x[2], x[4] = 1, 0.5
 
     # Random mixing matrix
-    R = np.random.normal(0.0, 1.0, (n, m))
+    R = rng.normal(0.0, 1.0, (n, m))
     Rop = MatrixMult(R)
-    y = Rop @ x
-    l2 = L2(Op=Rop, b=y, niter=50, warm=True)
 
-    g = np.random.normal(0.0, 1.0, m)
-    l1 = L1(sigma=5e-1, g=g)
+    y = Rop @ x
+
+    l2 = L2(Op=Rop, b=y, niter=50, warm=True)
+    l1 = L1(sigma=5e-1)
 
     # Step size
     L = (Rop.H * Rop).eigs(1).real.item()
     tau = 0.5 / L
 
-    # DRS
-    xdrs = DouglasRachfordSplitting(l2, l1, x0=np.zeros(m), tau=tau, niter=15000, show=True)
+    xdrs, _ = DouglasRachfordSplitting(
+        l2, l1, x0=np.zeros(m), tau=tau, niter=15000, show=True)
+    xppxa1 = PPXA(
+        [l2, l1], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-7)
 
-    # PPXA
-    xppxa = PPXA(
-        [l2, l1], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-4
-    )
+    assert_array_almost_equal(xdrs, xppxa1, decimal=2)
 
-    assert_array_almost_equal(xdrs, xppxa, decimal=2)
+    xppxa2 = PPXA(
+        [l1, l2], x0=np.zeros(m), tau=tau, niter=15000, show=True, tol=1e-7)
+
+    assert_array_almost_equal(xdrs, xppxa2, decimal=2)
